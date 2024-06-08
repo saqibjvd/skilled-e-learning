62aa1889c3200962a5240103e2764735a3d65250
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2024-06-08 15:04:34 +0100

    start of project
    h1 added

---
+++ app/page.js
@@ -0,0 +1,18 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Skilled e-Learning</h1>
+      <h2>Skilled e-learning landing page</h2>
+      <p>
+        Your challenge is to build out this landing page and get it looking as
+        close to the design as possible. You can use any tools you like to help
+        you complete the challenge. <br></br>
+        So if you've got something you'd like to practice, feel free to give it
+        a go. Your users should be able to: View the optimal layout depending on
+        their device's screen size See hover states for interactive elements
+      </p>
+    </div>
+  );
+}
